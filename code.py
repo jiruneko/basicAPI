4df825cd4436c94f9d95cc75c2899cd953c1291e
jiruneko <oohikaru310@gmail.com>
2022-08-02 07:49:43 +0900

    first commit

---
+++ code.py
@@ -0,0 +1,6 @@
+import requests
+res = requests.get('https://zipcloud.ibsnet.co.jp/api/search?zipcode=1000001')
+res_json = res.json()
+results = res_json['results'][0]
+address = results['address1'] + results['address2'] + results['address3']
+print(address)
